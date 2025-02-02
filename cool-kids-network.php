36ac1506305068b6bc62c6b043715ca11cce8d54
Satish Prajapati <satishprajapati91@gmail.com>
2025-02-02 22:51:11 +0530

    Phpcs Setup and Code linting
    Setup phpcs
    Code linting using phpcs and phpcbf

--- cool-kids-network.php
+++ cool-kids-network.php
@@ -8,7 +8,7 @@
  */
 
 
- // Prevent direct access to this file
+// Prevent direct access to this file
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
--- includes/class-cool-kids-network.php
+++ includes/class-cool-kids-network.php
@@ -1,8 +1,8 @@
 <?php
 namespace CoolKidsNetwork;
 
- // Prevent direct access to this file
- if ( ! defined( 'ABSPATH' ) ) {
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
@@ -20,44 +20,56 @@ class Cool_Kids_Network {
 	 * @var null|object $instance
 	 */
 
-	 private static $_instance = null;
+	private static $_instance = null;
 
-	 /**
-	  * The function will return the instance of the class.
-	  *
-	  * @access public
-	  * @since  0.1
-	  * @return @var $instance
-	  */
-	 public static function init() {
-		 if ( null === self::$_instance ) {
-			 self::$_instance = new Cool_Kids_Network();
-		 }
-		 return self::$_instance;
-	 }
- 
-	 /**
-	  * The constructor of the class.
-	  *
-	  * @access public
-	  * @since  0.1
-	  * @return void
-	  */
-	 public function __construct() {
-		 // Enqueue JavaScript for AJAX
-		 add_action('wp_enqueue_scripts', array($this, 'enqueue_scripts'));
-	 }
+	/**
+	 * The function will return the instance of the class.
+	 *
+	 * @access public
+	 * @since  0.1
+	 * @return @var $instance
+	 */
+	public static function init() {
+		if ( null === self::$_instance ) {
+			self::$_instance = new Cool_Kids_Network();
+		}
+		return self::$_instance;
+	}
 
-	 /**
-	  * Enqueue required Javascript and CSS files of the plugin
-	  *
-	  * @access public
-	  * @since  0.1
-	  * @return void
-	  */
-	 public function enqueue_scripts() {
-		wp_enqueue_style('ckn-style', CKN_PATH . 'assets/css/style.css', [], CKN_VERSION);
-	 }
+	/**
+	 * The constructor of the class.
+	 *
+	 * @access public
+	 * @since  0.1
+	 * @return void
+	 */
+	public function __construct() {
+		$this->setup();
+		// Enqueue JavaScript for AJAX
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+	}
+
+	/**
+	 * Enqueue required Javascript and CSS files of the plugin
+	 *
+	 * @access public
+	 * @since  0.1
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		wp_enqueue_style( 'ckn-style', CKN_PATH . 'assets/css/style.css', [], CKN_VERSION );
+	}
+
+	/**
+	 * Setup the necessary files.
+	 *
+	 * @access private
+	 * @since  0.1
+	 * @return void
+	 */
+	private function setup() {
+		new Roles();
+	}
 }
 
 Cool_Kids_Network::init();
